626ad73a48e440f821b73fe88bfe616960ea32cd
ordaz00 <ordazjose00@gmail.com>
2023-04-27 16:14:10 -0700

    removed the preprocessing as it was unecessary, put the columns in the correct order, and added a column that I was missing to correctly format the dataframe

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,86 +1,35 @@
 import pandas as pd
-import re
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 
-#function to format the csv file
+# Function to format the csv file
 def formatCSV(file):
-    #read in file
+    # Read in file
     df = pd.read_csv(file, header=None)
 
-    #add in column names
+    # Add in column names
     df.rename(columns={0: 'ids', 1: 'user', 2: 'sentiment score', 3: 'text'}, inplace=True)
 
-    #drop unecessary columns
+    # Drop unecessary columns
     df = df.drop(columns=['ids','user'])
 
-    #remove irrelevant rows
+    # Remove irrelevant rows
     df.drop(df[df["sentiment score"] == "Irrelevant"].index, inplace=True)
 
-    #convert sentiment from string to numbers
-    df['sentiment score'] = df['sentiment score'].replace(["Negative", "Neutral", "Positive"], [0,1,2])
+    # Convert sentiment from string to numbers
+    df['sentiment'] = df['sentiment score'].replace(["Negative", "Neutral", "Positive"], [0,1,2])
+
+    # Switch the order of the columns so they are in the correct order
+    df = df[['text', 'sentiment']]
+
+    # Add a max sentiment column
+    df['sentimentMax'] = 2
 
     return df
-
-# Function to preprocess the text
-def preprocess_text(text):
-   # Handle non-string values
-   if not isinstance(text, str):
-       return ""
-  
-   # Remove URLs
-   text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
-
-
-   # Remove special characters, numbers, and punctuation
-   text = re.sub(r'\W', ' ', text)
-
-
-   # Remove extra spaces
-   text = re.sub(r'\s+', ' ', text)
-
-
-   # Convert to lowercase
-   text = text.lower()
-
-
-   # Tokenize the text
-   tokens = nltk.word_tokenize(text)
-
-
-   # Remove stop words
-   stop_words = set(stopwords.words('english'))
-
-
-   tokens = [word for word in tokens if word not in stop_words]
-
-
-   # Lemmatize the tokens
-   lemmatizer = WordNetLemmatizer()
-
-
-   tokens = [lemmatizer.lemmatize(word) for word in tokens]
-
-
-   # Join the tokens back into a single string
-   text = ' '.join(tokens)
-
-
-   return text
 
 # Grab the formated dataframes
 training = formatCSV("twitter_training.csv")
 
 
 validation = formatCSV("twitter_validation.csv")
-
-
-# Apply the preprocessing function to the text column
-training['text'] = training['text'].apply(preprocess_text)
-
-
-validation['text'] = validation['text'].apply(preprocess_text)
 
 
 # Saving the dataframes to csv's
